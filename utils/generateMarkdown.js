d973142dc5064fbb4824b175236076b7d014bdca
Hannahphoto <hannahprice67@gmail.com>
2023-11-19 22:11:16 -0500

    organized commented out criteria

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+//WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+// which license the application is covered under
+// WHEN I enter my GitHub username
 function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
